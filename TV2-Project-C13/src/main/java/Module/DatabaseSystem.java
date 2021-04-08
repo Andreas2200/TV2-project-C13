47464f2515dce09277e8d05ba1fab15c8c22e0e7
SSpedsbjerg <71696995+SSpedsbjerg@users.noreply.github.com>
2021-04-08 18:27:35 +0200

    Har nogle problemer med Intellij, så ved ikke om min kode vil virke som det skal, det her er bare så i kan se hvad der er blevet lavet indtil videre, I løbet af i aften vil der blive lavet et passwprd system og gjort at DatabaseSystem er en Singleton

---
+++ TV2-Project-C13/src/main/java/Module/DatabaseSystem.java
@@ -0,0 +1,104 @@
+package Module;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+class Person{
+    int age;
+    int ID;
+    String Email;
+    String Name;
+    Person(int age, int ID, String Email, String Name){
+        this.age = age; this.ID = ID; this.Email = Email; this.Name = Name;
+    }
+    public String getName(){
+        return Name;
+    }
+}
+
+class Main {
+    static DatabaseSystem dbSys = new DatabaseSystem();
+    public static void main(String[] args) throws Exception {
+        ArrayList<Person> searched = dbSys.SearchPerson("k");
+        for (Person person : searched){
+            System.out.println("Person:\n" + person.Name + "\nAge:\n" + person.age + "\nEmail:\n" + person.Email);
+        }
+    }
+}
+
+public class DatabaseSystem {
+    public String Search(int id) throws Exception {
+        try {
+            ArrayList<String> readLines = new ArrayList<>();
+            Scanner reader = new Scanner(new File("C:/Users/simon/IdeaProjects/Sorting/src/sample/this.txt"));
+            while (reader.hasNextLine()){
+                readLines.add(reader.nextLine());
+            }
+
+        }
+        catch (Exception e){
+            throw new Exception();
+        }
+        return "";
+    }
+
+    //SearchParam uses letters to narrow the search result down, type is the searched var in object, file is the file the data is in
+    public ArrayList<Person> SearchPerson(String searchParam) throws Exception {
+        //readlines and put em in a list
+        String splitValue = "";
+        searchParam = searchParam.toLowerCase();
+        ArrayList<Person> searchItems = new ArrayList<>();
+        ArrayList<Person> dataValues = getAllPersons();
+
+        //throw out all of the lines which is not searched for
+        for (int i = 0; i < dataValues.size(); i++){
+            splitValue = SplitByChar(dataValues.get(i).Name, searchParam.length());
+            splitValue = splitValue.toLowerCase();
+            if (splitValue.equals(searchParam)){
+                searchItems.add(dataValues.get(i));
+                System.out.println(dataValues.get(i).Name);
+            }
+        }
+        //sort em
+        Collections.sort(searchItems, Comparator.comparing(Person::getName));
+        return searchItems;
+    }
+
+    //An unsorted and quicker way to get all persons, mostly used by Datasystem itself but can be used outside
+    public ArrayList<Person> getAllPersons() throws Exception {
+        ArrayList<Person> persons = new ArrayList<>();
+        ArrayList<String> personValues = readDataValues("D:/Memes/Sorting/Sorting/out/production/Sorting/sample/this.txt");
+        for (int i = 0; i < personValues.size(); i = i + 4){
+            persons.add(new Person(Integer.parseInt(personValues.get(i + 2)), Integer.parseInt(personValues.get(i + 0)), personValues.get(i + 3), personValues.get(i + 1)));
+        }
+        return persons;
+    }
+
+
+    private String SplitByChar(String text, int splitBy){
+        return text.substring(0, splitBy);
+    }
+
+    private ArrayList<String> readDataValues(String path) throws Exception {
+        ArrayList<String> readLines = new ArrayList<>();
+        ArrayList<String> returnLines = new ArrayList<>();
+        try {
+            Scanner reader = new Scanner(new File(path));
+            while (reader.hasNextLine()){
+                readLines.add(reader.nextLine());
+            }
+            for (String readLine : readLines){
+                String[] lines = readLine.split(",");
+                for (String line : lines){
+                    returnLines.add(line);
+                }
+            }
+        }
+        catch (Exception e){
+            throw new FileNotFoundException();
+        }
+        return returnLines;
+    }
+
+}
