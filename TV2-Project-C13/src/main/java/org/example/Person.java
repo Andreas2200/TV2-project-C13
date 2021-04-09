fe9a0491b8ab3fae98e4deec2764580189facd58
tobber00 <39513708+tobber00@users.noreply.github.com>
2021-04-09 09:16:47 +0200

    lavede klasser

---
+++ TV2-Project-C13/src/main/java/org/example/Person.java
@@ -0,0 +1,31 @@
+package org.example;
+
+public class Person {
+    private int age;
+    private int id;
+    private String email;
+    private String name;
+
+    public Person (int age, int id, String email, String name) {
+        this.age = age;
+        this.id = id;
+        this.email = email;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
